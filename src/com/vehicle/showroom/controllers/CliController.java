d00a3240c9e606db16d7f52f74e3281943207b04
anwar-arif <anwarhossain221095@gmail.com>
2020-05-13 19:31:49 +0600

    change package path

--- src/com/vehicle/showroom/controllers/CliController.java
+++ src/com/vehicle/showroom/controllers/CliController.java
@@ -69,6 +69,9 @@ public class CliController {
 
             System.out.println();
         }
+        if (vehicles.isEmpty()) {
+            System.out.println("No vehicles to show");
+        }
     }
 
     /**
